67487a7c16fa1a18317ab40b8f59a24366da8805
Justin Schneck <jschneck@mac.com>
2022-11-23 08:34:37 -0500

    Remove erlang/elixir depend on upstream BR

--- package/peridio/peridiod/peridiod.mk
+++ package/peridio/peridiod/peridiod.mk
@@ -6,15 +6,16 @@
 
 PERIDIOD_VERSION = 1.0.0
 PERIDIOD_SITE = $(call github,peridio,peridiod,v$(PERIDIOD_VERSION))
-PERIDIOD_DEPENDENCIES = host-peridio-elixir peridio-erlang
+PERIDIOD_DEPENDENCIES = host-elixir host-erlang erlang
+PERIDIOD_ERTS_DIR = $(wildcard $(STAGING_DIR)/usr/lib/erlang/erts-*)
 
 define PERIDIOD_BUILD_CMDS
     $(TARGET_MAKE_ENV) \
-      PERIDIOD_INCLUDE_ERTS_DIR=$(STAGING_DIR)/usr/lib/erlang/erts-13.0.3 \
+      PERIDIOD_INCLUDE_ERTS_DIR=$(PERIDIOD_ERTS_DIR) \
       ERL_CFLAGS="-I$(STAGING_DIR)/lib/erlang/usr/include -I$(STAGING_DIR)/usr/include/libmnl" \
       ERL_LDFLAGS="-L$(STAGING_DIR)/usr/lib -lmnl -L$(STAGING_DIR)/lib/erlang/usr/lib -lei" \
       MIX_ENV=prod \
-      MIX_TARGET=not_host \
+      MIX_TARGET=target \
       $(MAKE) \
       $(TARGET_CONFIGURE_OPTS) \
       -C $(@D) release
@@ -23,7 +24,8 @@ endef
 define PERIDIOD_INSTALL_TARGET_CMDS
     ln -sf /usr/lib/peridiod/bin/peridiod $(TARGET_DIR)/usr/sbin/peridiod
     cp -r $(@D)/_build/not_host_prod/rel/peridiod $(TARGET_DIR)/usr/lib
-    cp $(PERIDIOD_PKGDIR)/peridiod.service $(TARGET_DIR)/etc/systemd/system/peridiod.service
+    mkdir -p $(TARGET_DIR)/etc/systemd/system
+    cp $(PERIDIOD_PKGDIR)peridiod.service $(TARGET_DIR)/etc/systemd/system/peridiod.service
 endef
 
 $(eval $(generic-package))
