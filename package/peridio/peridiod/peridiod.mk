1a6f8a08ccf2ad5c841adc1b9dfb9a5cb03e0856
Justin Schneck <jschneck@mac.com>
2022-11-30 13:33:37 -0500

    Fix compile locale warnings

--- package/peridio/peridiod/peridiod.mk
+++ package/peridio/peridiod/peridiod.mk
@@ -11,6 +11,8 @@ PERIDIOD_ERTS_DIR = $(wildcard $(STAGING_DIR)/usr/lib/erlang/erts-*)
 
 define PERIDIOD_BUILD_CMDS
     $(TARGET_MAKE_ENV) \
+      LANG=en_US.UTF-8 \
+      LC_ALL=en_US.UTF-8 \
       PERIDIOD_INCLUDE_ERTS_DIR=$(PERIDIOD_ERTS_DIR) \
       ERL_CFLAGS="-I$(STAGING_DIR)/lib/erlang/usr/include -I$(STAGING_DIR)/usr/include/libmnl" \
       ERL_LDFLAGS="-L$(STAGING_DIR)/usr/lib -lmnl -L$(STAGING_DIR)/lib/erlang/usr/lib -lei" \
@@ -23,7 +25,7 @@ endef
 
 define PERIDIOD_INSTALL_TARGET_CMDS
     ln -sf /usr/lib/peridiod/bin/peridiod $(TARGET_DIR)/usr/sbin/peridiod
-    cp -r $(@D)/_build/not_host_prod/rel/peridiod $(TARGET_DIR)/usr/lib
+    cp -r $(@D)/_build/target_prod/rel/peridiod $(TARGET_DIR)/usr/lib
     mkdir -p $(TARGET_DIR)/etc/systemd/system
     cp $(PERIDIOD_PKGDIR)peridiod.service $(TARGET_DIR)/etc/systemd/system/peridiod.service
 endef
